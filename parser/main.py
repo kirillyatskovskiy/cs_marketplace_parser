81e900016ee6dd297682b379004fa8dc77afe577
kirill.yatskovskiy <k.yatskovskiy.work@gmail.com>
2025-02-12 18:46:50 +0300

    extended script to work with additional keys and store them in the database

--- parser/main.py
+++ parser/main.py
@@ -20,9 +20,24 @@ def create_table():
         CREATE TABLE IF NOT EXISTS cs2_market (
             id SERIAL PRIMARY KEY,
             name TEXT,
-            price INTEGER,
-            price_text TEXT,
-            image_url TEXT
+            hash_name TEXT,
+            sell_listings INTEGER,
+            sell_price INTEGER,
+            sell_price_text TEXT,
+            app_icon TEXT,
+            app_name TEXT,
+            appid INTEGER,
+            classid TEXT,
+            instanceid TEXT,
+            icon_url TEXT,
+            tradable INTEGER,
+            item_name TEXT,
+            name_color TEXT,
+            item_type TEXT,
+            market_name TEXT,
+            market_hash_name TEXT,
+            commodity INTEGER,
+            sale_price_text TEXT
         )
     """)
     conn.commit()
@@ -30,13 +45,20 @@ def create_table():
     conn.close()
 
 # Функция для вставки данных в базу
-def insert_item(name, price, price_text, image_url):
+def insert_item(data):
     conn = psycopg2.connect(**DB_CONFIG)
     cursor = conn.cursor()
     cursor.execute("""
-        INSERT INTO cs2_market (name, price, price_text, image_url)
-        VALUES (%s, %s, %s, %s)
-    """, (name, price, price_text, image_url))
+        INSERT INTO cs2_market (name, hash_name, sell_listings, sell_price, sell_price_text, app_icon, app_name,
+                                appid, classid, instanceid, icon_url, tradable, item_name, name_color, item_type, 
+                                market_name, market_hash_name, commodity, sale_price_text)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """, (
+        data["name"], data["hash_name"], data["sell_listings"], data["sell_price"], data["sell_price_text"],
+        data["app_icon"], data["app_name"], data["appid"], data["classid"], data["instanceid"], 
+        data["icon_url"], data["tradable"], data["item_name"], data["name_color"], data["item_type"], 
+        data["market_name"], data["market_hash_name"], data["commodity"], data["sale_price_text"]
+    ))
     conn.commit()
     cursor.close()
     conn.close()
@@ -90,13 +112,33 @@ def parse_market():
         results = data.get("results", [])
         
         for item in results:
-            name = item.get("name", "Unknown")
-            price = item.get("sell_price", 0)
-            price_text = item.get("sell_price_text", "N/A")
-            image_url = f"https://community.akamai.steamstatic.com/economy/image/{item.get('asset_description', {}).get('icon_url', '')}"
-            insert_item(name, price, price_text, image_url)
+            # Извлечение данных из ответа
+            item_data = {
+                "name": item.get("name", "Unknown"),
+                "hash_name": item.get("hash_name", "Unknown"),
+                "sell_listings": item.get("sell_listings", 0),
+                "sell_price": item.get("sell_price", 0),
+                "sell_price_text": item.get("sell_price_text", "N/A"),
+                "app_icon": item.get("app_icon", ""),
+                "app_name": item.get("app_name", "Unknown"),
+                "appid": item.get("asset_description", {}).get("appid", 0),
+                "classid": item.get("asset_description", {}).get("classid", ""),
+                "instanceid": item.get("asset_description", {}).get("instanceid", ""),
+                "icon_url": item.get("asset_description", {}).get("icon_url", ""),
+                "tradable": item.get("asset_description", {}).get("tradable", 0),
+                "item_name": item.get("asset_description", {}).get("name", "Unknown"),
+                "name_color": item.get("asset_description", {}).get("name_color", ""),
+                "item_type": item.get("asset_description", {}).get("type", ""),
+                "market_name": item.get("asset_description", {}).get("market_name", ""),
+                "market_hash_name": item.get("asset_description", {}).get("market_hash_name", ""),
+                "commodity": item.get("asset_description", {}).get("commodity", 0),
+                "sale_price_text": item.get("sale_price_text", "N/A")
+            }
+            
+            # Вставка данных в базу
+            insert_item(item_data)
         
-        print(f"Загружено {start + len(results)} из {total_items} предметов")
+        print(f"Loaded {start + len(results)} of {total_items} items")
         time.sleep(2)  # Пауза, чтобы избежать блокировки Steam API
 
 if __name__ == "__main__":
