fb27ca7b572fddb244ba0922da5d657282740533
kirill.yatskovskiy <k.yatskovskiy.work@gmail.com>
2025-02-12 22:57:12 +0300

    fix: Added volume cleanup step to prevent issues with persistent data. Issue was caused by not clearing volumes, leading to unexpected behavior. Apologies for the oversight, but that's just an excuse! <3

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@
 echo "Stopping Docker containers..."
 docker compose down
 
+# Очистка volumes
+echo "Removing Docker volume..." 
+docker volume rm $(docker volume ls -qf name=postgres_data_cs2p)
+
 # Обновление репозитория
 echo "Pulling latest changes from Git..."
 git pull
