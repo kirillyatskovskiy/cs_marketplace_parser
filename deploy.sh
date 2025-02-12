ef353e19e81290b8067a8a85d98fe45959d0c5ff
kirill.yatskovskiy <k.yatskovskiy.work@gmail.com>
2025-02-12 22:28:44 +0300

    added deploy automatization script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Остановка контейнеров Docker
+echo "Stopping Docker containers..."
+docker compose down
+
+# Обновление репозитория
+echo "Pulling latest changes from Git..."
+git pull
+
+# Строим и запускаем контейнеры Docker с обновлением
+echo "Building and starting Docker containers..."
+docker compose up --build -d
+
+# Завершаем выполнение
+echo "Deployment complete!"
