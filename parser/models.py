9b8bad2e5c08d80da2020847ddfc7fa3e9b7f426
kirill.yatskovskiy <k.yatskovskiy.work@gmail.com>
2025-02-16 03:10:29 +0300

    Remove unused imports - Cleaned up code by removing unnecessary and unused import statements. - Improved code readability and reduced potential for confusion or errors.

--- parser/models.py
+++ parser/models.py
@@ -1,8 +1,5 @@
 from main import MODE
-from sqlalchemy import create_engine, Column, Integer, String, Text
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import NullPool
+from sqlalchemy import Column, Integer, Text
 from databse import Base
 
 # Определение модели для таблицы cs2_market
