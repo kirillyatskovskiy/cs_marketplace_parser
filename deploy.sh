4bbbf45e6526a4d9cf1ad88f53d8edda4f56ab0c
kirill.yatskovskiy <k.yatskovskiy.work@gmail.com>
2025-02-16 17:58:48 +0300

    feat: persist database data, remove automatic volume deletion Removed the code block that deleted the Docker volume postgres_data_cs2p to prevent accidental database deletion. The data will now persist permanently.

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,6 @@ docker compose down
 # Диалог выбора, очищать ли volume для Postgres
 read -p "Do you want to remove the Postgres volume? (y/n): " remove_volume
 
-if [ "$remove_volume" == "y" ]; then
-  echo "Removing Docker volume..."
-  docker volume rm $(docker volume ls -qf name=postgres_data_cs2p)
-else
-  echo "Skipping Docker volume removal."
-fi
-
 # Обновление репозитория
 echo "Pulling latest changes from Git..."
 git pull
